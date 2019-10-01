23ffcd389269ac9dc14c2b8413d10edf182b5bb1
joadva <43792106+joadva@users.noreply.github.com>
2019-10-01 18:04:49 -0500

    Update prenderfoco.ino

--- miercoles/ejercicios/prenderfoco.ino
+++ miercoles/ejercicios/prenderfoco.ino
@@ -9,3 +9,19 @@ void loop() {
   digitalWrite(foco, LOW);    
   delay(1000);                      
 }
+
+//Prender led con jhoony five uwu js :v 
+/*
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function() {
+
+  // Create a standard `led` component instance
+  var led = new five.Led(13);
+
+  // "blink" the led in 500ms
+  // on-off phase periods
+  led.blink(500);
+});
+*/ 
